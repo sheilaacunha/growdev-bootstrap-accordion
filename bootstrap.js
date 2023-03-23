7dbfa55b4b3ad25bb112a4d2433c196f4a5010f3
Sheila Acunha <sheilaacunha1990@gmail.com>
2023-03-22 22:47:39 -0300

    ok

--- bootstrap.js
+++ bootstrap.js
@@ -45,43 +45,52 @@ const lista = [
     {
         codigo: '#01',
         nome: 'Terminar as tarefas.',
-        detalhamento: 'Fazer todos os trabalhos e tarefas pendentes.',
-        cor1: 'bg-warning',
-        opacidade: 'bg-opacity-50',
+        detalhamento: 'Fazer todos os trabalhos e tarefas pendentes.'
     },
     {
         codigo: '#02',
         nome: 'Aprender JavaScript',
-        detalhamento: 'Olhar os vídeos de js.',
-        cor1: 'bg-secondary',
-        opacidade: 'bg-opacity-50',
+        detalhamento: 'Olhar os vídeos de js.'
     },
     {
         codigo: '#03',
         nome: 'Terminar o trabalho de módulo final !',
-        detalhamento: 'Colocar animações e Bootstrap',
-        cor1: 'bg-success',
-        opacidade: 'bg-opacity-50',
+        detalhamento: 'Colocar animações e Bootstrap'
+
     }]
 
 for (const item of lista) {
-    document.getElementById('accordionFlushExample').innerHTML += `
-    
-    <div class="accordion-item">
-          <h2 class="accordion-header" id="flush-headingOne">
-            <button class="accordion-button collapsed  ${item.cor1}" type="button" data-bs-toggle="collapse"
-              data-bs-target="${item.codigo}" aria-expanded="false" aria-controls="flush-collapseOne">
-              <strong>${item.nome} ${item.codigo}</strong>
-            </button>
-          </h2>
-          <div id="id${item.codigo}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
-            data-bs-parent="#accordionFlushExample">
-            <div class="accordion-body  ${item.cor1} ${item.opacidade}">${item.detalhamento}
-            </div>
-          </div>
-        </div>
+    document.getElementById('accordionFlushExample').innerHTML +=
 
+        `
+    <div class="accordion-item">
+    <h2 class="accordion-header" id="${item.codigo}">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+      <strong>${item.nome} ${item.codigo}</strong>
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="${item.codigo}" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">${item.detalhamento} .</div>
+    </div>
+  </div>
     `
+
+    // `   
+    // <div class="accordion-item">
+    //       <h2 class="accordion-header" id="flush-headingOne">
+    //         <button class="accordion-button collapsed  ${item.cor1}" type="button" data-bs-toggle="collapse"
+    //           data-bs-target="${item.codigo}" aria-expanded="false" aria-controls="flush-collapseOne">
+    //           <strong>${item.nome} ${item.codigo}</strong>
+    //         </button>
+    //       </h2>
+    //       <div id="id${item.codigo}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+    //         data-bs-parent="#accordionFlushExample">
+    //         <div class="accordion-body  ${item.cor1} ${item.opacidade}">${item.detalhamento}
+    //         </div>
+    //       </div>
+    //     </div>
+
+    // `
 }
 
 
